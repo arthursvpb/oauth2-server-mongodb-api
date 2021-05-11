3b17f1c559a3ae11efd91ef764d1c6816ad5ed4a
arthursvpb <arthurandroid15@gmail.com>
2021-05-11 17:05:26 -0300

    add bcryptjs to hash user password

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,6 +1,8 @@
 import User from '../models/User';
 import { Request, Response } from 'express';
 
+import { hash } from 'bcryptjs';
+
 const UserController = {
   async index(req: Request, res: Response) {
     try {
@@ -15,8 +17,10 @@ const UserController = {
   async create(req: Request, res: Response) {
     const { body } = req;
 
+    const hashedPassword = await hash(body.password, 8);
+
     try {
-      const user = await User.create(body);
+      const user = await User.create({ ...body, password: hashedPassword });
 
       return res.status(200).json({
         message: `✅ User created successfully!`,
