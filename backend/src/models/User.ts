9a33d04d3fa7ea433a66fd7dde82ee258361cd63
arthursvpb <arthurandroid15@gmail.com>
2021-05-12 18:56:53 -0300

    add balance to user

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -12,6 +12,7 @@ const UserSchema: Schema = new Schema(
     username: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     password: { type: String, required: true },
+    balance: { type: String, default: 100000 },
   },
   {
     timestamps: true,
